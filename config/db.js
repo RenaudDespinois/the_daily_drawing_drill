0b2a163a7bac01f6a94c24f1a72423c89d09b71b
RenaudDespinois <renaud.despinois@gmail.com>
2013-06-07 13:57:43 +0200

    Adding missing files

---
+++ config/db.js
@@ -0,0 +1,15 @@
+/**
+ * ------------------------------------------------
+ * db.js : Defines the db constant and exports them
+ * ------------------------------------------------
+ */
+
+module.exports = {
+	user: "webuser",
+	pass: "12webuserpwd34",
+	name: "the_daily_drawing_drill",
+	host: "dharma.mongohq.com",
+	port: 10039
+}
+
+
